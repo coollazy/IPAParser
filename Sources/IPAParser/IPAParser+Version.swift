080edf25a7a34e10873d1de52102d72120823f55
Ven <ven.wu@baifu-tech.net>
2025-12-15 12:02:39 +0800

    [Refactor] 優化讀取版本號直接套用 PlistParser

--- Sources/IPAParser/IPAParser+Version.swift
+++ Sources/IPAParser/IPAParser+Version.swift
@@ -1,16 +1,11 @@
 import Foundation
+import PlistParser
 
 public extension IPAParser {
     func version() -> String? {
         guard let infoPlistURL = try? appDirectory().appendingPathComponent("Info.plist") else {
             return nil
         }
-        guard let plistData = try? Data(contentsOf: infoPlistURL) else {
-            return nil
-        }
-        guard let plistInfos = try? PropertyListSerialization.propertyList(from: plistData, format: nil) as? [String: Any] else {
-            return nil
-        }
-        return plistInfos["CFBundleShortVersionString"] as? String
+        return try? PlistParser(url: infoPlistURL).get(keyPath: "CFBundleShortVersionString") as? String
     }
 }
